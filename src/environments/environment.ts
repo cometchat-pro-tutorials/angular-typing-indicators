fb751bed6b0fad8c9c2d8eaadb7647318b4a52e9
Maciej Treder <contact@maciejtreder.com>
2019-08-29 12:34:14 +0200

    removing appid and apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  appId: '2744976a01ce18',
-  apiKey: 'd80672cd36f7f01d97630e26f2c32dfd5f9c2182'
+  appId: 'your-appId',
+  apiKey: 'your-apiKey'
  };
