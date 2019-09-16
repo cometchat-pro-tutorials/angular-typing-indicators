51d161c1c2e34ac3857c5f865d1d405a9fa09fe3
Taha Shashtari <taha.shashtari@gmail.com>
2019-09-16 18:27:01 +0300

    improve styling

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  appId: 'your-appId',
-  apiKey: 'your-apiKey'
+  appId: '782728541f19b4',
+  apiKey: '24db762631c327f1e033fb71859335e7a46b31f9'
  };
